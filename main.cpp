87dc6b46e322e9e7c160f182457bbcddd4caf9af
xhvozdov <xhvozdov@mendelu.cz>
2024-12-02 14:08:50 +0100

    CLS

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 
 
 #include "Room.h"
 #include "Player.h"
+
+#ifdef _WIN32
+#define CLEAR "cls"
+#define RESET "mode con: cols=1 lines=1 && mode con: cols=80 lines=25"
+#else
+#define CLEAR "clear"
+#define RESET "reset"
+#endif
 
 int main() {
     // Nastavení generátoru náhodných čísel
@@ -17,12 +26,10 @@ int main() {
 
     while (true) {
         player->Move(room);
+        system(CLEAR);
+        system(RESET);
+
         room->printRoom();
-        std::cout<<room->getRoom().size()<<std::endl;
-        std::cout<<"X: "<<player->getPositionX()<<std::endl;
-        std::cout<<"Y: "<<player->getPositionY()<<std::endl;
-
-
     }
 
 
