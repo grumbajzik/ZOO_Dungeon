c54e61425b8656a1e9d2c648686ae1a90240ce76
Fajec <Mich.Faj@email.cz>
2024-12-13 22:20:27 +0100

    funkční exitbutton 4. část

--- Menu/NewGameButton.cpp
+++ Menu/NewGameButton.cpp
@@ -5,16 +5,64 @@
 #include "NewGameButton.h"
 
 NewGameButton::NewGameButton() {
+    m_indexOfPlayer = 1;
+    m_newPlayer.push_back("Archer");
+    m_newPlayer.push_back("Warrior");
+
 }
 
 void NewGameButton::execute() {
-    system("cls");
-    std::cout << "Choose a character." << std::endl;
+    switchPlayer();
+    createHero();
 
 }
 
 void NewGameButton::drawButton() {
     std::cout << "Start a new game." << std::endl;
+}
+
+void NewGameButton::switchChoice(char input) {
+    input = static_cast<int>(input);
+    switch (input) {
+        case 72: { //up
+            if (m_indexOfPlayer > 0) {
+                m_indexOfPlayer--;
+            }
+            break;
+        }
+        case 80: {  // down
+            if (m_indexOfPlayer < 3) {
+                m_indexOfPlayer++;
+            }
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+void NewGameButton::switchPlayer() {
+    system("cls");
+    char input;
+    while (input != '\r') {
+        input = _getch();
+        switchChoice(input);
+        system("cls");
+        std::cout << "CHOOSE YOUR CLASS" << std::endl << std::endl;
+
+        for (int i = 0; i < m_newPlayer.size(); i++) {
+            if (i == m_indexOfPlayer){
+                std::cout << "->";
+            } else {
+                std::cout << "  ";
+            }
+            std::cout << m_newPlayer[i] << std::endl << std::endl;
+        }
+    }
+}
+
+Player* NewGameButton::createHero(PlayerFactory *playerFactory) {
+    playerFactory->createPlayer(m_indexOfPlayer);
 }
 
 
--- Menu/NewGameButton.h
+++ Menu/NewGameButton.h
@@ -9,13 +9,24 @@
 #include "Button.h"
 #include <conio.h>
 
+#include "../Player/PlayerFactory.h"
+
 class NewGameButton : public Button{
     std::vector<std::string> m_newPlayer;
     int m_indexOfPlayer;
+    void switchChoice(char input);
+    void switchPlayer();
+
 public:
+    Player* createHero(PlayerFactory* playerFactory);
     NewGameButton();
     void drawButton() override;
+
+
+
+
     void execute() override;
+
 };
 
 
